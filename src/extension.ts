0c8714ae5c1513c8b57a15fab3b3b4fe2d0871cf
jigkoxsee <supakorn@jigko.net>
2020-02-16 23:01:44 +0700

    Remove hello world

--- src/extension.ts
+++ src/extension.ts
@@ -6,16 +6,11 @@ import { BuildFeedProvider } from './buildFeedProvider';
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
-		vscode.window.showInformationMessage('Hello World!');
-	});
 
-	// Samples of `window.registerTreeDataProvider`
-	const buildFeedProvider = new BuildFeedProvider(); //vscode.workspace.rootPath
+	const buildFeedProvider = new BuildFeedProvider();
 	vscode.window.registerTreeDataProvider('droneBuildFeed', buildFeedProvider);
 	vscode.commands.registerCommand('droneBuildFeed.refreshEntry', () => buildFeedProvider.refresh());
 
-	context.subscriptions.push(disposable);
 	console.log('Congratulations, your extension "droneci" is now active!');
 }
 
